c0672a00301b869dfee84372072ee8dfadef0ac2
zack <zackaryfotheringham@hotmail.com>
2022-11-21 10:19:06 -0500

    display winner and restart game

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let iterationCount = 0
 let gameBoard = ['','','','','','','','','']
+let gameOver = false
 
 const GameBoard = (e)=>{
+    const playerOne = 'Player 1'
+    const playerTwo = 'Player 2'
+    let currentPlayer = playerOne
     const gridPieces = document.querySelectorAll('div.box')
     if (e.target.textContent === 'X' || e.target.textContent === 'O'){
         e.target.classList.add('shake')
@@ -10,27 +14,33 @@ const GameBoard = (e)=>{
         e.target.classList.add('depress')
         setTimeout(function(){e.target.classList.remove('depress')},300)
         if (iterationCount %2 == 0){
+            e.target.style.color = '#808080'
             updateGameBoard('X')
-            UpdatePlayer('Player 2')
-            
+            currentPlayer = playerOne
+            updatePlayer(playerTwo)
         } else {
             updateGameBoard('O')
-            UpdatePlayer('Player 1')
+            currentPlayer = playerTwo
+            updatePlayer(playerOne)
         }
     }
     function updateGameBoard(gameVariable){
         for(let i=0;i<gridPieces.length;i++){
-            if(e.target == gridPieces[i]){
-                gameBoard.splice(i, 0,gameVariable)
+            if(e.target === gridPieces[i]){
+                gameBoard.splice(i, 1, gameVariable)
                 e.target.textContent = gameVariable
             }
         }
     }
+    function updatePlayer(player){
+        const displayPlayer = document.getElementsByClassName('display-player-box')[0]
+        displayPlayer.textContent = `Your Turn ${player}`
+    }
     iterationCount++
-    if (iterationCount > 4){checkForWin()}
+    if (iterationCount > 4){checkForWin(currentPlayer)}
 }
 
-const checkForWin = ()=>{
+const checkForWin = (player)=>{
     const gameBoardGroupOne = [gameBoard[0],gameBoard[1],gameBoard[2]],
     gameBoardGroupTwo = [gameBoard[3],gameBoard[4],gameBoard[5]],
     gameBoardGroupThree = [gameBoard[6],gameBoard[7],gameBoard[8]],
@@ -43,38 +53,23 @@ const checkForWin = ()=>{
         let isEmpty = false
         for(let i=0;i<arr.length;i++){
             if(arr[i] === ''){
-                console.log(arr[i])
                 isEmpty = true
                 return false
             }
-            if (isEmpty === false){
-               return new Set(arr).size === 1
-            }
         }
+        if (isEmpty === false){return new Set(arr).size === 1}
     }
-    if (allTheSame(gameBoardGroupOne) === true){
-        console.log('winner')
-    } else if (allTheSame(gameBoardGroupTwo) === true){
-        console.log('winner 2')
-    } else if (allTheSame(gameBoardGroupThree) === true){
-        console.log('winner 3')
-    } else if (allTheSame(gameBoardGroupFour) === true){
-        console.log('winner 4')
-    } else if (allTheSame(gameBoardGroupFive) === true){
-        console.log('winner 5')
-    } else if (allTheSame(gameBoardGroupSix) === true){
-        console.log('winner 6')
-    } else if (allTheSame(gameBoardGroupSeven) === true){
-        console.log('winner 7')
-    } else if (allTheSame(gameBoardGroupEight) === true){
-        console.log('winner 8')
+    if (allTheSame(gameBoardGroupOne) === true || allTheSame(gameBoardGroupTwo) === true || allTheSame(gameBoardGroupThree) === true ||
+    allTheSame(gameBoardGroupFour) === true || allTheSame(gameBoardGroupFive) === true || allTheSame(gameBoardGroupSix) === true ||
+    allTheSame(gameBoardGroupSeven) === true || allTheSame(gameBoardGroupEight) === true){
+        const winBox = document.getElementsByClassName('win-container')[0]
+        winBox.classList.add('show-modal')
+        document.getElementsByClassName('win-header')[0].textContent = `${player} Wins!`
+        setTimeout(()=>{
+            setTimeout(()=>{winBox.classList.remove('show-modal')},1500)
+            DisplayController.fullRestart()
+        },2000)
     }
-    
-}
-
-const UpdatePlayer = (player) => {
-    const displayPlayer = document.getElementsByClassName('display-player-box')[0]
-    displayPlayer.textContent = `Your Turn ${player}`
 }
 
 const DisplayController = (()=>{
@@ -83,60 +78,41 @@ const DisplayController = (()=>{
     giveUpButton = document.getElementsByClassName('give-up-button')[0],
     giveUpButtonConfirm = document.getElementsByClassName('confirm-button')[0],
     quitModal = document.getElementsByClassName('quit-container')[0],
-    modeModal = document.getElementsByClassName('mode-container')[0],
-    pvpButton = document.getElementsByClassName('pvp-mode')[0],
-    pveButton = document.getElementsByClassName('pve-mode')[0],
     contentBoxes = document.querySelectorAll('div.box')
-    let isRestarted = false
     contentBoxes.forEach(box => box.addEventListener('click',e=>{GameBoard(e)}))
-    startButton.addEventListener('click',()=>{modeModal.classList.add('show-modal')})
-    pvpButton.addEventListener('click', switchToModal)
-    pveButton.addEventListener('click', switchToModal)
+    startButton.addEventListener('click',startGame)
     giveUpButton.addEventListener('click',()=>{quitModal.classList.add('show-modal')})
-    giveUpButtonConfirm.addEventListener('click', ()=>{
-        startButton.style.visibility = 'visible'
-        giveUpButton.style.visibility = 'hidden'
-        displayPlayerBox.style.visibility = 'hidden'
-        quitModal.classList.remove('show-modal')  
-        isRestarted = true
-        emptyGameBoard()
-    })
-
-    function switchToModal(){
+    giveUpButtonConfirm.addEventListener('click',restartGame)
+    function startGame(){
         startButton.style.visibility = 'hidden'
-        modeModal.classList.remove('show-modal')
         displayPlayerBox.style.visibility = 'visible' 
         giveUpButton.style.visibility = 'visible'
         setTimeout(()=>{contentBoxes.forEach(box => box.style.pointerEvents = 'all')},1350)
         emptyGameBoard()
     }
-
     function emptyGameBoard(){
-        const timeout = 150
         for(let i=0;i<contentBoxes.length;i++){
             emptyBoxes(i)
         }
         function emptyBoxes(i){
-            setTimeout(function(){
+            setTimeout(()=>{
                 contentBoxes[i].textContent = ''
-            },timeout*i)
-        }
-        if (isRestarted == true){
-            for (i=0;i<contentBoxes.length;i++){
-                fillBoxes(i)
-            }
-            function fillBoxes(i){
-                setTimeout(function(){
-                    contentBoxes[i].textContent = 'X'
-                },timeout*i)
-            }
-            isRestarted = false
+            },150*i)
         }
     }
+    function restartGame(){
+        startButton.style.visibility = 'visible'
+        giveUpButton.style.visibility = 'hidden'
+        displayPlayerBox.style.visibility = 'hidden'
+        quitModal.classList.remove('show-modal')
+        gameBoard = ['','','','','','','','','']
+        iterationCount = 0
+        emptyGameBoard()
+        setTimeout(()=>{contentBoxes.forEach(box => box.textContent = 'X')},1350)
+        setTimeout(()=>{contentBoxes.forEach(box => box.style.color = 'white')},1350)
+        setTimeout(()=>{contentBoxes.forEach(box => box.style.pointerEvents = 'none')},1350)
+    }
+    return {
+        fullRestart: restartGame
+    }
 })()
-
-
-const test = ['1','','3']
-const test2 = [test[0],test[2]]
-
-console.log(test2[0])
